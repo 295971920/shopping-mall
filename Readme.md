c91aabbeefb53086c351aa2e027d667dadb4e178
xiaoshan <295971920@qq.com>
2020-05-23 00:07:30 +0800

    App.vue引入全局公共样式

--- Readme.md
+++ Readme.md
@@ -1,6 +1,10 @@
-App.Vue 引入全局公共样式
-	1、引入样式库
+1、App.Vue 引入全局公共样式
+	1.1、引入样式库
 		uni.css 	//官方ui库
 		animate.css //css动画库
 		icon.css 	//图标库
 		common.css 	//公共样式
+2、解决uni.css第五行找不到uni.ttf报错
+	将uni.ttf拷贝到static目录下即可解决
+3、解决iconfont.css中.iconfont报错
+	font-family: "iconfont"  , sans-serif  !important;
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,7 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+		<view style="font-size: 50px;"  class="iconfont">&#xe62b;</view>
+		<view style="font-size: 50px;"  class="iconfont icon-VIP"></view>
 	</view>
 </template>
 
@@ -11,7 +9,7 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				
 			}
 		},
 		onLoad() {
@@ -24,29 +22,4 @@
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
 </style>
